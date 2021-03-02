dd33232e9a60fb3500d6399976d1895d05116ee5
chloew96 <chloew090600@gmail.com>
2021-03-01 22:04:06 -0500

    first commit

---
+++ precache-manifest.45bce58d325cc1e3d3b23234d53ae8e2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb433f368afdea56349725bcccf2ad8c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0e89561b573423134f9e",
+    "url": "./static/css/main.6bbff01c.chunk.css"
+  },
+  {
+    "revision": "ea2315f9a3e8923175e0",
+    "url": "./static/js/2.5c855351.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.5c855351.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e89561b573423134f9e",
+    "url": "./static/js/main.30503c28.chunk.js"
+  },
+  {
+    "revision": "0561679ec34ef3768e8a",
+    "url": "./static/js/runtime-main.5a023ad9.js"
+  },
+  {
+    "revision": "fc46048f7b60a7c995cc65f439b202fd",
+    "url": "./static/media/antibioticbg.fc46048f.jpg"
+  },
+  {
+    "revision": "b7fbe437a54a1d676a51faae53ba9c5d",
+    "url": "./static/media/biomimeticbg.b7fbe437.jpg"
+  },
+  {
+    "revision": "26850efa6bef96b545cbd4764ab25e59",
+    "url": "./static/media/biomimeticfig.26850efa.png"
+  },
+  {
+    "revision": "bde103dce4d489cca7e68a261c33fa92",
+    "url": "./static/media/biosynthbg.bde103dc.jpg"
+  },
+  {
+    "revision": "2cb303aa47fe2ae8255c9f8cf2d42138",
+    "url": "./static/media/biosynthfig.2cb303aa.png"
+  },
+  {
+    "revision": "b1180d166eb0429eef8a06afe30f314b",
+    "url": "./static/media/cover.b1180d16.png"
+  },
+  {
+    "revision": "ba442c779a0f9d74b971a5ce7827bab9",
+    "url": "./static/media/footermaclogo.ba442c77.png"
+  },
+  {
+    "revision": "1aa81ab03aa43d582c12c733e089b37a",
+    "url": "./static/media/grouppic.1aa81ab0.jpg"
+  },
+  {
+    "revision": "07f2330887d36405e1331acef7dc0aad",
+    "url": "./static/media/linkedinicon.07f23308.png"
+  },
+  {
+    "revision": "82bfcf13a647d1f2c17e4a12c3dcac5d",
+    "url": "./static/media/navlogo.82bfcf13.png"
+  },
+  {
+    "revision": "c352ec93cb0e34519f12731c7b988d4a",
+    "url": "./static/media/piprofile.c352ec93.png"
+  }
+]);
